c83fe591278e19579623f0dd8a7f7f887ca51b71
KyleBPerez <star_wars51389@yahoo.com>
2022-03-08 13:49:59 -0800

    added CONST DB_NAME

--- db/client.js
+++ db/client.js
@@ -1,7 +1,9 @@
 const { Client } = require('pg')
 
+const DB_NAME = 'pet-rocks-db'
+
 const CONNECTION_STRING =
-  process.env.DATABASE_URL || 'postgres://localhost:5432/pet-rocks-db'
+  process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`
 
 const client = new Client(CONNECTION_STRING)
 
