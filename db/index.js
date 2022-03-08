2d641210e47fc7f1ff3cf86f7386ccb3fb5ed27f
KyleBPerez <star_wars51389@yahoo.com>
2022-03-07 21:41:20 -0800

    initial set up based off  boilerplate

---
+++ db/index.js
@@ -0,0 +1,17 @@
+/* 
+users table
+  id | username | password
+
+products table 
+  id | name | price | description
+
+image table
+  id | image | url | productId
+
+reviews table
+  id | name | description | review poster | productId
+
+Comments table
+ id | comment poster | description | reviewId
+
+*/
