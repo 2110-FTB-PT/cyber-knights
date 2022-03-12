e047eec9ac74856e5bef767be7365f5900e83c89
KyleBPerez <star_wars51389@yahoo.com>
2022-03-12 12:29:13 -0800

    client var added

--- db/client.js
+++ db/client.js
@@ -5,19 +5,19 @@ const DB_NAME = 'pet-rocks-db'
 const CONNECTION_STRING =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`
 
-// if (process.env.CI) {
-//   client = new Client({
-//     host: 'localhost',
-//     port: 5432,
-//     user: 'postgres',
-//     password: 'postgres',
-//     database: 'postgres',
-//   })
-// } else {
-//   // local / heroku client config
-//   client = new Client(CONNECTION_STRING)
-// }
+let client
 
-const client = new Client(CONNECTION_STRING)
+if (process.env.CI) {
+  client = new Client({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: 'postgres',
+    database: 'postgres',
+  })
+} else {
+  // local / heroku client config
+  client = new Client(CONNECTION_STRING)
+}
 
 module.exports = client
