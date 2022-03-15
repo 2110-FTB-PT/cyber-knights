9b3364615a3b9bc71f44595fbaef4e7b8e8dcb04
maywong2021 <y.may.wong@gmail.com>
2022-03-14 18:45:13 -0700

    added api routers for users

--- api/index.js
+++ api/index.js
@@ -13,6 +13,40 @@ apiRouter.get('/health', (req, res, next) => {
   })
 })
 
+apiRouter.use(async (req, res, next) => {
+  const prefix = "Bearer ";
+  const auth = req.header("Authorization");
+
+  if (!auth) {
+    next();
+  } else if (auth.startsWith(prefix)) {
+    const token = auth.slice(prefix.length);
+
+    try {
+      const { id } = jwt.verify(token, JWT_SECRET);
+
+      if (id) {
+        req.user = await getUserById(id);
+        next();
+      }
+    } catch ({ name, message }) {
+      next({ name, message });
+    }
+  } else {
+    next({
+      name: "AuthorizationHeaderError",
+      message: `Authorization token must start with ${prefix}`,
+    });
+  }
+});
+
+apiRouter.use((req, res, next) => {
+  if (req.user) {
+    console.log("User is set:", req.user);
+  }
+  next();
+});
+
 // place your routers here
 apiRouter.use("/users", usersRouter);
 
