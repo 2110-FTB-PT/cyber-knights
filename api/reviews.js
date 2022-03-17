f36ff49c83143f74816597f850f85358f7c02a73
KyleBPerez <star_wars51389@yahoo.com>
2022-03-16 20:39:13 -0700

    added reviewsRouters
    seperated image table to review_images and product_images
    
    re orginized root index.js
    
    removed server check
    
    deleted apiRouter.test because it was not being used

--- api/reviews.js
+++ api/reviews.js
@@ -7,68 +7,63 @@ const {
   getReviewsByProduct,
   getProductById,
 } = require('../db')
+const { requireUser } = require('./utils')
 
 //require user to create a new review
-reviewsRouter.post(
-  '/create',
-  /* requireUser, */ async (req, res, next) => {
-    const { isPublic, title, description, productId } = req.body
-    const { id } = req.user
+reviewsRouter.post('/create', requireUser, async (req, res, next) => {
+  const { isPublic, title, description, productId } = req.body
+  const { id } = req.user
 
-    if (!title || !description) {
-      next({
-        name: `CreateReviewErr`,
-        message: `Must complete all fields to create a post`,
-      })
-      return
-    }
-    try {
-      const newReview = await createReview({
-        userId: id,
-        isPublic,
-        title,
-        description,
-        productId,
-      })
-
-      res.send(newReview)
-    } catch (err) {
-      next(err)
-    }
+  if (!title || !description) {
+    next({
+      name: `CreateReviewErr`,
+      message: `Must complete all fields to create a post`,
+    })
+    return
   }
-)
+  try {
+    const newReview = await createReview({
+      userId: id,
+      isPublic,
+      title,
+      description,
+      productId,
+    })
 
-// also needs a user to be logged in and be the original
+    res.send(newReview)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// require user to be logged in and be the original
 // creator of the review  to change/update
-reviewsRouter.patch(
-  '/:reviewId',
-  /* requireUser, */ async (req, res, next) => {
-    const { reviewId: id } = req.params
-    const { isPublic, title, description } = req.body
-    const updateField = { id }
+reviewsRouter.patch('/:reviewId', requireUser, async (req, res, next) => {
+  const { reviewId: id } = req.params
+  const { isPublic, title, description } = req.body
+  const updateField = { id }
 
-    if (isPublic) updateField.isPublic = isPublic
+  if (isPublic) updateField.isPublic = isPublic
 
-    if (title) updateField.title = title
+  if (title) updateField.title = title
 
-    if (description) updateField.description = description
+  if (description) updateField.description = description
 
-    try {
-      const originalReview = await getReviewById(id)
-      if (originalReview.userId === 'req.user.id') {
-        const updatedReview = await updateReview(updateField)
-        res.send(updatedReview)
-      } else {
-        next({
-          name: `UnauthorizedUserErr`,
-          message: `Can not update review unless you are the owner`,
-        })
-      }
-    } catch (err) {
-      next(err)
+  try {
+    const originalReview = await getReviewById(id)
+    if (originalReview.userId === req.user.id) {
+      const updatedReview = await updateReview(updateField)
+      res.send(updatedReview)
+    } else {
+      next({
+        name: `UnauthorizedUserErr`,
+        message: `Can not update review unless you are the owner`,
+      })
     }
+  } catch (err) {
+    next(err)
   }
-)
+})
 
 reviewsRouter.get('/:productId', async (req, res, next) => {
   const { productId } = req.params
--- index.js
+++ index.js
@@ -1,53 +1,32 @@
-// This is the Web Server
 const express = require('express')
 const server = express()
-require("dotenv").config()
-
-// enable cross-origin resource sharing to proxy api requests
-// from localhost:3000 to localhost:4000 in local dev env
+require('dotenv').config()
+const { client } = require('./db')
 const cors = require('cors')
-server.use(cors())
-
-// create logs for everything
 const morgan = require('morgan')
-server.use(morgan('dev'))
-
-// handle application/json requests
-server.use(express.json())
-
-// here's our static files
 const path = require('path')
+const PORT = process.env.PORT || 4000
+
+server.use(cors())
+server.use(morgan('dev'))
+server.use(express.json())
 server.use(express.static(path.join(__dirname, 'build')))
 
-// here's our API
 server.use('/api', require('./api'))
 
-// by default serve up the react app if we don't recognize the route
 server.use((req, res, next) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-// bring in the DB connection
-const { client } = require('./db')
-
-// connect to the server
-const PORT = process.env.PORT || 4000
-
-server.use(({ name, message }, req, res, next) => {
-  res.status(500).send({ name, message });
-});
-
-// define a server handle to close open tcp connection after unit tests have run
-const handle = server.listen(PORT, async () => {
-  console.log(`Server is running on ${PORT}!`)
-
-  try {
-    await client.connect()
-    console.log('Database is open for business!')
-  } catch (error) {
-    console.error('Database is closed for repairs!\n', error)
-  }
+server.get('*', (req, res, next) => {
+  res.status(404).send('PAGE NOT FOUND')
 })
 
-// export server and handle for routes/*.test.js
-module.exports = { server, handle }
+server.use(({ name, message }, req, res, next) => {
+  res.status(500).send({ name, message })
+})
+
+server.listen(PORT, () => {
+  client.connect()
+  console.log(`Server is running on ${PORT}`)
+})
