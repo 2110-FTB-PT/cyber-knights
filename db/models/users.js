2aa81e37b61d3266509ec1e65aca5850c1528151
KyleBPerez <star_wars51389@yahoo.com>
2022-03-19 13:37:18 -0700

    added Admin table and seeded admin accounts

--- db/models/users.js
+++ db/models/users.js
@@ -1,9 +1,10 @@
-const client = require("../client");
-const bcrypt = require("bcrypt");
-const SALT_ROUNDS = 10;
+const client = require('../client')
+const bcrypt = require('bcrypt')
+const SALT_ROUNDS = 10
 
 async function createUser({ username, password }) {
-  const hashPwd = await bcrypt.hash(password, SALT_ROUNDS);
+  const hashPwd = await bcrypt.hash(password, SALT_ROUNDS)
+
   try {
     const {
       rows: [user],
@@ -15,12 +16,35 @@ async function createUser({ username, password }) {
             RETURNING *;
           `,
       [username, hashPwd]
-    );
-    
-    delete user.password;
-    return user;
+    )
+
+    delete user.password
+    return user
   } catch (error) {
-    throw error;
+    throw error
+  }
+}
+
+async function createAdmin({ username, password }) {
+  const hashPwd = await bcrypt.hash(password, SALT_ROUNDS)
+
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+            INSERT INTO admins(username, password) 
+            VALUES($1, $2) 
+            ON CONFLICT (username) DO NOTHING 
+            RETURNING *;
+          `,
+      [username, hashPwd]
+    )
+
+    delete user.password
+    return user
+  } catch (error) {
+    throw error
   }
 }
 
@@ -35,28 +59,28 @@ async function getUser({ username, password }) {
             WHERE username=$1;
           `,
       [username]
-    );
+    )
 
     if (!user) {
       throw {
-        name: "UserDoesNotExist",
-        message: "User does not exist",
-      };
+        name: 'UserDoesNotExist',
+        message: 'User does not exist',
+      }
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password)
 
     if (isMatch) {
-      delete user.password;
-      return user;
+      delete user.password
+      return user
     } else {
       throw {
-        name: "IncorrectPassword",
-        message: "Password provided was incorrect, Please try again",
-      };
+        name: 'IncorrectPassword',
+        message: 'Password provided was incorrect, Please try again',
+      }
     }
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
@@ -71,15 +95,15 @@ async function getUserById(id) {
           WHERE id=$1;
         `,
       [id]
-    );
+    )
 
     if (!user) {
-      return null;
+      return null
     }
 
-    return user;
+    return user
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
@@ -94,16 +118,17 @@ async function getUserByUsername(username) {
           WHERE username=$1;
         `,
       [username]
-    );
-    return user;
+    )
+    return user
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
 module.exports = {
   createUser,
+  createAdmin,
   getUser,
   getUserById,
   getUserByUsername,
-};
+}
