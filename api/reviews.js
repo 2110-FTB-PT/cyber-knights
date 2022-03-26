70957e8b4e8dbe50a66dcb41bc84f52f35c9870e
KyleBPerez <star_wars51389@yahoo.com>
2022-03-26 14:03:17 -0700

    added GET '/myReviews` api route to get all the reviews for a user

--- api/reviews.js
+++ api/reviews.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const reviewsRouter = express.Router()
+const express = require("express");
+const reviewsRouter = express.Router();
 const {
   createReview,
   updateReview,
   getReviewById,
   getReviewsByProduct,
   getProductById,
-} = require('../db')
-const { requireUser } = require('./utils')
+} = require("../db");
+const { requireUser } = require("./utils");
 
 //require user to create a new review
-reviewsRouter.post('/create', requireUser, async (req, res, next) => {
-  const { isPublic, title, description, productId } = req.body
-  const { id } = req.user
+reviewsRouter.post("/create", requireUser, async (req, res, next) => {
+  const { isPublic, title, description, productId } = req.body;
+  const { id } = req.user;
 
   if (!title || !description) {
     next({
       name: `CreateReviewErr`,
       message: `Must complete all fields to create a post`,
-    })
-    return
+    });
+    return;
   }
   try {
     const newReview = await createReview({
@@ -28,60 +28,68 @@ reviewsRouter.post('/create', requireUser, async (req, res, next) => {
       title,
       description,
       productId,
-    })
+    });
 
-    res.send(newReview)
+    res.send(newReview);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // require user to be logged in and be the original
 // creator of the review  to change/update
-reviewsRouter.patch('/:reviewId', requireUser, async (req, res, next) => {
-  const { reviewId: id } = req.params
-  const { isPublic, title, description } = req.body
-  const updateField = { id }
+reviewsRouter.patch("/:reviewId", requireUser, async (req, res, next) => {
+  const { reviewId: id } = req.params;
+  const { isPublic, title, description } = req.body;
+  const updateField = { id };
 
-  if (isPublic) updateField.isPublic = isPublic
+  if (isPublic) updateField.isPublic = isPublic;
 
-  if (title) updateField.title = title
+  if (title) updateField.title = title;
 
-  if (description) updateField.description = description
+  if (description) updateField.description = description;
 
   try {
-    const originalReview = await getReviewById(id)
+    const originalReview = await getReviewById(id);
     if (originalReview.userId === req.user.id) {
-      const updatedReview = await updateReview(updateField)
-      res.send(updatedReview)
+      const updatedReview = await updateReview(updateField);
+      res.send(updatedReview);
     } else {
       next({
         name: `UnauthorizedUserErr`,
         message: `Can not update review unless you are the owner`,
-      })
+      });
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-reviewsRouter.get('/:productId', async (req, res, next) => {
-  const { productId } = req.params
+reviewsRouter.get("/:productId", async (req, res, next) => {
+  const { productId } = req.params;
   try {
-    const product = await getProductById(productId)
+    const product = await getProductById(productId);
     if (!product) {
       next({
         name: `NoProductFound`,
         message: `This product could NOT be found`,
-      })
-      return
+      });
+      return;
     }
 
-    const productReviews = await getReviewsByProduct(productId)
-    res.send(productReviews)
+    const productReviews = await getReviewsByProduct(productId);
+    res.send(productReviews);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-module.exports = reviewsRouter
+reviewsRouter.get("/myReviews", requireUser, async (req, res, next) => {
+  const { id } = req.user;
+  try {
+    const userReviews = await getPublicReviewsByUser(id);
+    res.send(userReviews);
+  } catch (err) {}
+});
+
+module.exports = reviewsRouter;
