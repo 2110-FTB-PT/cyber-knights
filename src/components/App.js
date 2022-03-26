5593890606dd1833bee1f7d76c122878e46c76fb
maywong2021 <y.may.wong@gmail.com>
2022-03-26 12:44:15 -0700

    created login/register forms and buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import Header from "./Header";
 import Home from "./Home";
+import Login from "./Login";
 import "../style/App.css";
 import { Route, Routes } from "react-router-dom";
+import { getUser } from "../axios-services";
 
 const App = () => {
+  const [token, setToken] = useState("");
+  const [user, setUser] = useState({});
+  console.log("user", user);
+
+  useEffect(() => {
+    const handleUser = async () => {
+      if (token) {
+        const userInfo = await getUser(token);
+        console.log('userInfo :>> ', userInfo);
+        setUser(userInfo);
+      }
+    };
+
+    handleUser();
+  }, [token]);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"));
+    }
+  }, []);
+
   return (
     <div className="app-container">
-      <Header />
+      <Header username={user && user.username} token={token} setToken={setToken} setUser={setUser}/>
       <div className="content-container d-flex justify-content-center">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -17,7 +41,10 @@ const App = () => {
             path="/products-accessories"
             element={<h1>products-accessories</h1>}
           />
-          <Route path="/login" element={<h1>login</h1>} />
+          <Route
+            path="/login"
+            element={<Login setToken={setToken} setUser={setUser} />}
+          />
           <Route path="/cart" element={<h1>cart</h1>} />
         </Routes>
       </div>
