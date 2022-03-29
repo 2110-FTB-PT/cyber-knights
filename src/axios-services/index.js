a65034e4d779d0072d0492a0bdfe2e104433f590
KyleBPerez <star_wars51389@yahoo.com>
2022-03-28 22:22:37 -0700

    changed axios urls to BASE_URL

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -1,34 +1,28 @@
 import axios from "axios";
-const {PORT} = process.env
 const BASE_URL = "https://build-a-rock.herokuapp.com/api";
-const TEST_URL = `http://localhost:3000/api`
-console.log(PORT);
 
 export const fetchProducts = async () => {
   try {
-    const {data} = await axios.get(`${TEST_URL}/products/public`);
-    return data
+    const { data } = await axios.get(`${BASE_URL}/products/public`);
+    return data;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 };
 
-export const fetchProductById = async(id) => {
+export const fetchProductById = async (id) => {
   try {
-    const {data} = await axios.get(`${TEST_URL}/products/${id}`);
+    const { data } = await axios.get(`${BASE_URL}/products/${id}`);
     console.log(data);
-    return data
+    return data;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-  
 };
-
-
 
 export const login = async (username, password) => {
   try {
-    const { data } = await axios.post(`${TEST_URL}/users/login`, {
+    const { data } = await axios.post(`${BASE_URL}/users/login`, {
       username,
       password,
     });
@@ -40,11 +34,11 @@ export const login = async (username, password) => {
 };
 
 export const register = async (username, password) => {
-  try{
-    const { data } = await axios.post(`${TEST_URL}/users/register`, {
-    username,
-    password,
-  });
+  try {
+    const { data } = await axios.post(`${BASE_URL}/users/register`, {
+      username,
+      password,
+    });
     return data;
   } catch (error) {
     throw error;
@@ -53,29 +47,29 @@ export const register = async (username, password) => {
 
 export const getUser = async (token) => {
   try {
-    const { data } = await axios.get(`${TEST_URL}/users/me`, {
+    const { data } = await axios.get(`${BASE_URL}/users/me`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
-    console.log(data)
+    console.log(data);
     return data;
-  } catch ({response}) {
-    console.error({response}) ;
+  } catch ({ response }) {
+    console.error({ response });
   }
 };
 
-export const fetchUserComments = async(token) => {
+export const fetchUserComments = async (token) => {
   try {
     const { data } = await axios.get(`${BASE_URL}/comments/myComments`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
     return data;
-  } catch ({response}) {
-    console.error("response", response)
+  } catch ({ response }) {
+    console.error("response", response);
     // throw error;
   }
 };
@@ -94,20 +88,24 @@ export const fetchUserReviews = async (token) => {
   }
 };
 
-export const updateReviewComment = async ({ id: commentId, ...commentFields }) => {
-  try{
+export const updateReviewComment = async ({
+  id: commentId,
+  ...commentFields
+}) => {
+  try {
     const { data } = await axios.patch(
       `${BASE_URL}/comments/${commentId}`,
       {
-        ...commentFields
+        ...commentFields,
       },
-       {headers: {
+      {
+        headers: {
           Authorization: `Bearer ${token}`,
         },
-      });
+      }
+    );
     return data;
   } catch (error) {
     throw error;
   }
 };
-
