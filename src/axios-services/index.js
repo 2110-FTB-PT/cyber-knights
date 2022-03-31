f22318aff22a12b2afb9113686cb8e2450361f2d
KyleBPerez <star_wars51389@yahoo.com>
2022-03-31 14:42:06 -0700

    changed axios back to eherokuapp URL

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "http://build-a-rock.herokuapp.com/api";
 
 export const fetchProducts = async () => {
   try {
