effa8575b4bdbfd1e5499599a75e82316ff84bc3
AaaronPM <aaron.perez989@gmail.com>
2022-04-02 13:25:56 -0700

    added shopping cart redirects and style
    header  with logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,7 +2,7 @@ import '../style/logo.css'
 export default function Logo() {
   return(
     <div className="logo-main">
-      <img src='' alt=''/>
+     <img id='main-image'src='https://i.imgur.com/ynQomPt.png' alt='asd'/>
     </div>
   )
 };
